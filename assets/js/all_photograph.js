302e5d737c3a7649d8d956f7d143943569f03045
Gautier De Coninck <gautier.deconinck@gmail.com>
2021-05-04 14:42:42 +0200

    add js for photographers list + tags

--- assets/js/all_photograph.js
+++ assets/js/all_photograph.js
@@ -1,10 +1,38 @@
-function showPhotographers () {
-    fetch ("/FishEyeDataFR.json")
-    .then(response => {
-        console.log(response.json())
+function showPhotographers() {
+  fetch("/FishEyeDataFR.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+      }
+      return response.json();
     })
-    .catch (function () {
-       console.log('error') 
+    .then((data) => {
+      setPhotographersHTML(data.photographers);
     })
+    .catch(function (error) {
+      console.log("error", error);
+    });
 }
-showPhotographers()
+showPhotographers();
+
+function setPhotographersHTML(photographers) {
+  console.log(photographers);
+  photographers.forEach((element) => {
+    let photographHTML = `
+        <article class="d-flex column">
+          <figure class="profil-pic"><a href="/pages/photograph.html"><img src="assets/img/Sample_Photos/Photographers_ID_Photos/${element.portrait}" alt="Profil de Kimi Keel" /></a></figure>
+          <div class="d-flex column profil-description">
+            <h2><a href="/pages/photograph.html">${element.name}</a></h2>
+            <h3>${element.city},  ${element.country}</h3>
+            <p>${element.tagline}</p>
+            <p class="price">${element.price}€/jour</p>
+            <ul class="d-flex row tags-container">`;
+    element.tags.forEach((tag) => {
+      photographHTML += `<li class="tags">#${tag}</li>`
+    });
+    photographHTML += `</ul>
+    </div>
+    </article>`;
+    document.querySelector(".photographers-list").insertAdjacentHTML("beforeend", photographHTML);
+  });
+}
