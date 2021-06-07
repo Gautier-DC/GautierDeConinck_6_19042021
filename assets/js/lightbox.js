4a1c73682488a398d53773f487a08ded55c27d74
Gautier De Coninck <gautier.deconinck@gmail.com>
2021-06-07 02:00:57 +0200

    Fixed : Eslint done on every js files, solve issue on w3c validator

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -9,20 +9,19 @@ export const Lightbox = () => {
   }
   // Set lightbox basics
   let currentMediaIndex;
-  const lightboxDiv = document.createElement("div");
-  lightboxDiv.classList.add("lightbox", "hidden");
+  const lightboxDiv = document.createElement('div');
+  lightboxDiv.classList.add('lightbox', 'hidden');
   lightboxDiv.setAttribute('aria-hidden', 'true')
   document.body.appendChild(lightboxDiv);
-  lightboxDiv.innerHTML = `<button class="lightbox__close"><i class="fas fa-times"></i></button>
-  <button class="lightbox__next"><i class="fas fa-chevron-right"></i></button>
-  <button class="lightbox__prev"><i class="fas fa-chevron-left"></i></button>
-  <div class="lightbox__container d-flex column">
+  lightboxDiv.innerHTML = `<button class='lightbox__close'><i class='fas fa-times'></i></button>
+  <button class='lightbox__next'><i class='fas fa-chevron-right'></i></button>
+  <button class='lightbox__prev'><i class='fas fa-chevron-left'></i></button>
+  <div class='lightbox__container d-flex column'>
   </div>`;
   
   const playInnerVideo = (e) => {
     let lightboxVideo = document.getElementsByClassName('lightbox-video')[0];
     e.preventDefault();
-    console.log(lightboxVideo);
     // toggle play/pause
     if(lightboxVideo.paused) { 
       lightboxVideo.play(); 
@@ -39,17 +38,17 @@ export const Lightbox = () => {
   // Close the lightbox on click and with escape
   const closeLightbox = (e) =>{
     e.preventDefault();
-    lightboxDiv.classList.add("hidden");
-    document.body.style.overflow = "auto";
+    lightboxDiv.classList.add('hidden');
+    document.body.style.overflow = 'auto';
     lightboxDiv.setAttribute('aria-hidden', 'true');
-    window.removeEventListener("keydown", handlePressPlay);
+    window.removeEventListener('keydown', handlePressPlay);
   }
-  window.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
       closeLightbox(e);
     }
   });
-  document.querySelector(".lightbox__close").onclick = (e) => {
+  document.querySelector('.lightbox__close').onclick = (e) => {
     closeLightbox(e);
   };
 
@@ -57,7 +56,7 @@ export const Lightbox = () => {
   // Next media
   const handleNext = (e) => {
     e.preventDefault();
-    window.removeEventListener("keydown", handlePressPlay);
+    window.removeEventListener('keydown', handlePressPlay);
     // if last media then select first one in list
     if (currentMediaIndex == medias.length - 1) {
       setLightboxContent(medias[0]);
@@ -70,19 +69,19 @@ export const Lightbox = () => {
   };
 
   // Navigate with keyboard or inner arrows
-  window.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowRight") {
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowRight') {
       handleNext(e);
     }
   });
-  document.querySelector(".lightbox__next").onclick = (e) => {
+  document.querySelector('.lightbox__next').onclick = (e) => {
     handleNext(e);
   };
 
   // Previous media
   const handlePrev = (e) => {
     e.preventDefault();
-    window.removeEventListener("keydown", handlePressPlay);
+    window.removeEventListener('keydown', handlePressPlay);
     // if first media then select last one in list
     if (currentMediaIndex == 0) {
       setLightboxContent(medias[medias.length - 1]);
@@ -95,48 +94,48 @@ export const Lightbox = () => {
   };
 
   // navigate with keyboard or inner arrows
-  window.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowLeft" || e.key === "Backspace") {
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowLeft') {
       handlePrev(e);
     }
   });
-  document.querySelector(".lightbox__prev").onclick = (e) => {
+  document.querySelector('.lightbox__prev').onclick = (e) => {
     handlePrev(e);
   };
 
   // Create content to display in the lightbox
-  let lightboxContainer = document.querySelector(".lightbox__container");
+  let lightboxContainer = document.querySelector('.lightbox__container');
   const setLightboxContent = (currentMedia) => {
-    if (currentMedia.dataset.mediatype === "video") {
+    if (currentMedia.dataset.mediatype === 'video') {
       lightboxContainer.innerHTML = `
-      <video class="lightbox-video" controls>
+      <video class='lightbox-video' controls>
       <source src=../assets/img/Sample_Photos/${currentMedia.dataset.foldername}/${currentMedia.dataset.videoname}.mp4
-      type="video/mp4">
+      type='video/mp4'>
       <source src=../assets/img/Sample_Photos/${currentMedia.dataset.foldername}/${currentMedia.dataset.videoname}.ogv
-      type="video/ogg">
+      type='video/ogg'>
       <source src=../assets/img/Sample_Photos/${currentMedia.dataset.foldername}/${currentMedia.dataset.videoname}.webm
-      type="video/webm">
+      type='video/webm'>
       Sorry, your browser doesn't support embedded videos.
       </video>
-      <h3 class="picture-description" id="picture-name">` + currentMedia.href.split('/')[7].replace(/_/g, ' ').slice(0, -4) + `</h3>`;      
-      window.addEventListener("keydown", handlePressPlay);      
+      <h3 class='picture-description' id='picture-name'>` + currentMedia.href.split('/')[7].replace(/_/g, ' ').slice(0, -4) + `</h3>`;      
+      window.addEventListener('keydown', handlePressPlay);      
     } else {
       lightboxContainer.innerHTML = `
-      <img src="${currentMedia.href}" alt="${currentMedia.alt}">
-      <h3 class="picture-description" id="picture-name">` + currentMedia.title + `</h3>`;
+      <img src='${currentMedia.href}' alt='${currentMedia.alt}'>
+      <h3 class='picture-description'></h3>`;
     }
   };
   // Get the media
-  const medias = Array.from(document.querySelectorAll(".jsMedia"));
+  const medias = Array.from(document.querySelectorAll('.jsMedia'));
   
   // Once you have the media create html for it
   for (let i = 0 ; i < medias.length ; i++ ) {
-    medias[i].addEventListener("click", (e) => {
+    medias[i].addEventListener('click', (e) => {
       e.preventDefault();
       mainWrapper.setAttribute('aria-hidden', 'true');
-      lightboxDiv.classList.remove("hidden");
+      lightboxDiv.classList.remove('hidden');
       lightboxDiv.setAttribute('aria-hidden', 'false');
-      document.body.style.overflow = "hidden";
+      document.body.style.overflow = 'hidden';
       currentMediaIndex = i;
       setLightboxContent(medias[currentMediaIndex]);
       lightboxDiv.focus();
--- assets/js/photograph.js
+++ assets/js/photograph.js
@@ -1,17 +1,17 @@
-import { Lightbox } from "./lightbox.js";
-import { setModal } from "./modal.js";
+import { Lightbox } from './lightbox.js';
+import { setModal } from './modal.js';
 //Global function to display page of the photographer
 const showPhotographerProfil = () => {
   // Get the right url and inject the id parameter
   const urlParams = new URLSearchParams(window.location.search);
-  const idParam = urlParams.get("id");
+  const idParam = urlParams.get('id');
   // Find the .json file
-  fetch("/FishEyeDataFR.json")
+  fetch('../../FishEyeDataFR.json')
     //promise for the response (here data)
     .then((response) => {
       // Display an error if note find or convert data in json
       if (!response.ok) {
-        throw new Error("HTTP error " + response.status);
+        throw new Error('HTTP error ' + response.status);
       }
       return response.json();
     })
@@ -22,7 +22,7 @@ const showPhotographerProfil = () => {
       if (idParam) {
         filteredPhotographer = data.photographers.find((photographer) => photographer.id == idParam);
       } else {
-        console.log("No id found");
+        console.log('No id found');
       }
       // Set the html of the profil
       setProfilHTML(filteredPhotographer);
@@ -30,19 +30,19 @@ const showPhotographerProfil = () => {
       // If there is the id parameters, then find media which has a common id
       if (idParam) {
         filteredMedia = data.media.filter((media) => media.photographerId == filteredPhotographer.id);
-        buildOptionsList(filteredMedia, filteredPhotographer.name.split(" ")[0]);
+        buildOptionsList(filteredMedia, filteredPhotographer.name.split(' ')[0]);
       } else {
-        console.log("No media found");
+        console.log('No media found');
       }
       // Set modal
       setModal();
       // Set the gallery part
-      displayGallery(filteredMedia, filteredPhotographer.name.split(" ")[0]);
+      displayGallery(filteredMedia, filteredPhotographer.name.split(' ')[0]);
       // Set the lightbox
       Lightbox();
     })
     .catch(function (error) {
-      console.log("error", error);
+      console.log('error', error);
     });
 };
 // Use the function define previously
@@ -50,46 +50,47 @@ showPhotographerProfil();
 
 // Function for the banner part which include main infoirmation about the photographer
 const setProfilHTML = (photographer) => {
+  document.getElementsByTagName('title')[0].innerText = `Fisheye - Profil de ${photographer.name}`;
   let profilHTML = `
-        <div class="photographer-description d-flex column">
+        <div class='photographer-description d-flex column'>
             <h1>${photographer.name}</h1>
-            <h2 id="location">${photographer.city}, ${photographer.country}</h2>
-            <p id="catchphrase">${photographer.tagline}</p>
-            <ul class="d-flex row tag-container">`;
+            <h2 id='location'>${photographer.city}, ${photographer.country}</h2>
+            <p id='catchphrase'>${photographer.tagline}</p>
+            <ul class='d-flex row tag-container'>`;
   // For each corresponding tag, set this html
   photographer.tags.forEach((tag) => {
-    profilHTML += `<li class="tags"><a href='/index.html?tag=${tag}'>#${tag}</a></li>`;
+    profilHTML += `<li class='tags'><a href='/index.html?tag=${tag}'>#${tag}</a></li>`;
   });
   profilHTML += `</ul>
       </div>
-      <button class="open-modal-btn" aria-pressed="false"><a href="#modal-photographer" class="js-modal">Contactez-moi</a></button>
-      <figure class="profil-pic">
-        <img src="/assets/img/Sample_Photos/Photographers_ID_Photos/${photographer.portrait}" alt="" />
+      <a href='#modal-photographer' class='open-modal-btn js-modal' aria-pressed='false' role='button'>Contactez-moi</a>
+      <figure class='profil-pic'>
+        <img src='/assets/img/Sample_Photos/Photographers_ID_Photos/${photographer.portrait}' alt='' />
       </figure>
-      <div class ="d-flex row" id="likes-price">
-      <span id="total-likes"></span>
-      <span id="price">${photographer.price}€ / jour</span>
+      <div class ='d-flex row' id='likes-price'>
+      <span id='total-likes'></span>
+      <span id='price'>${photographer.price}€ / jour</span>
       </div>`;
   // Inject the previous HTML in banner-container part
-  document.getElementById("banner-container").insertAdjacentHTML("beforeend", profilHTML);
+  document.getElementById('banner-container').insertAdjacentHTML('beforeend', profilHTML);
   // Add photographer's name in contact form
-  document.querySelector("#titlemodal").innerHTML = `Contactez-moi </br> ${photographer.name}`;
+  document.querySelector('#titlemodal').innerHTML = `Contactez-moi </br> ${photographer.name}`;
 };
 
 // Function in order to display the right gallery of media, corresponding to the called photographer
 const displayGallery = (media, folderName) => {
-  let gallery = document.getElementById("gallery");
-  gallery.innerHTML = "";
+  let gallery = document.getElementById('gallery');
+  gallery.innerHTML = '';
   let totalLikes = 0;
   media.forEach((elt, i) => {
     totalLikes += elt.likes;
     // Set the HTML common to neither video or picture
     let structureMediaHTML = `
-    <figure class="picture-card d-flex column">
-      <div class="picture-description d-flex row">
-        <h3 class="picture-name">${elt.title}</h3>
-        <p class="counter-like">${elt.likes}</p>
-        <i class="fas fa-heart heart" focusable="true" tabindex="0"></i>
+    <figure class='picture-card d-flex column'>
+      <div class='picture-description d-flex row'>
+        <h3 class='picture-name'>${elt.title}</h3>
+        <p class='counter-like'>${elt.likes}</p>
+        <i class='fas fa-heart heart' tabindex='0'></i>
       </div>
     </figure>
     `;
@@ -97,40 +98,40 @@ const displayGallery = (media, folderName) => {
     let mediaHTML;
     if (elt.image) {
       mediaHTML = `
-          <a href="../assets/img/Sample_Photos/${folderName}/${elt.image} " class="jsMedia"
-          tabindex="0"><img
-              src="../assets/img/Sample_Photos/${folderName}/${elt.image} "
-              alt="${elt.alt}"              
+          <a href='../assets/img/Sample_Photos/${folderName}/${elt.image} ' class='jsMedia'
+          tabindex='0'><img
+              src='../assets/img/Sample_Photos/${folderName}/${elt.image} '
+              alt='${elt.alt}'              
           /></a>
         `;
     } else if (elt.video) {
       // get only the name of the video
-      let videoName = elt.video.split(".")[0];
+      let videoName = elt.video.split('.')[0];
       //Create html of the media
       mediaHTML = `
-      <a data-folderName="${folderName}" data-videoName="${videoName}" data-mediaType="video" class="jsMedia" href="../assets/img/Sample_Photos/${folderName}/${videoName}.mp4" tabindex="0"
-      ><video width="250" tabindex="-1" >
+      <a data-folderName='${folderName}' data-videoName='${videoName}' data-mediaType='video' class='jsMedia' href='../assets/img/Sample_Photos/${folderName}/${videoName}.mp4' tabindex='0'
+      ><video width='250' tabindex='-1' >
         <source src=../assets/img/Sample_Photos/${folderName}/${videoName}.mp4
-              type="video/mp4">
+              type='video/mp4'>
         <source src=../assets/img/Sample_Photos/${folderName}/${videoName}.ogv
-              type="video/ogg">
+              type='video/ogg'>
         <source src=../assets/img/Sample_Photos/${folderName}/${videoName}.webm
-              type="video/webm">
+              type='video/webm'>
         Sorry, your browser doesn't support embedded videos.
         </video></a>        
         `;
     } else {
       // error message
-      structureMediaHTML = `<p id="errorMedia">Sorry we didn't find anything for this photographer</p>`;
+      structureMediaHTML = `<p id='errorMedia'>Sorry we didn't find anything for this photographer</p>`;
     }
     // Add it to gallery section
-    gallery.insertAdjacentHTML("beforeend", structureMediaHTML);
-    document.getElementsByClassName("picture-card")[i].insertAdjacentHTML("afterbegin", mediaHTML);
+    gallery.insertAdjacentHTML('beforeend', structureMediaHTML);
+    document.getElementsByClassName('picture-card')[i].insertAdjacentHTML('afterbegin', mediaHTML);
     // Animation and counterfor the like button on each media
-    const heart = document.getElementsByClassName("heart")[i];
+    const heart = document.getElementsByClassName('heart')[i];
     const likeButton = (e) => {
       e.preventDefault();
-      let counterLike = document.getElementsByClassName("counter-like")[i];
+      let counterLike = document.getElementsByClassName('counter-like')[i];
       // Add +1 on media counter
       elt.likes++;
       // Refresh likes of media counter
@@ -138,18 +139,18 @@ const displayGallery = (media, folderName) => {
       // Add +1 on total counter
       totalLikes++;
       // Refresh likes of media counter
-      document.getElementById("total-likes").innerHTML = totalLikes + " ♥";
+      document.getElementById('total-likes').innerHTML = totalLikes + ' ♥';
       // Set animation of the heart
-      if (heart.classList.contains("like-anim")) {
-        heart.classList.remove("like-anim");
+      if (heart.classList.contains('like-anim')) {
+        heart.classList.remove('like-anim');
       }
       setTimeout(function () {
-        heart.classList.add("like-anim");
+        heart.classList.add('like-anim');
       }, 10);
     }
-
-    heart.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
+    // Like on click and with enter
+    heart.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
         likeButton(e);
       }
     });
@@ -157,30 +158,32 @@ const displayGallery = (media, folderName) => {
       likeButton(e);
     };
   });
-  document.getElementById("total-likes").innerHTML = totalLikes + " ♥";
+  document.getElementById('total-likes').innerHTML = totalLikes + ' ♥';  
 };
 
+const optionWrapper = document.querySelector('.option-wrapper');
+const optionList = document.querySelector('.filters-options-list');
 // Deploy filters menu
-document.querySelector("#filters").addEventListener("click", function () {
-  this.querySelector(".option-wrapper").classList.toggle("open");
-  this.querySelector(".filters-options-list").classList.toggle("open");
+document.querySelector('#filters').addEventListener('click', function () {
+  this.querySelector('.option-wrapper').classList.toggle('open');
+  this.querySelector('.filters-options-list').classList.toggle('open');
 });
 
 // Use filters
-let filtersOptions = ["Popularité", "Date", "Titre"];
+let filtersOptions = ['Popularité', 'Date', 'Titre'];
 // Construct ordering possibilities
 const buildOptionsList = (medias, photographerName, orderedOptions = filtersOptions) => {
-  document.getElementById("filters-options-list").innerHTML = "";
+  document.getElementById('filters-options-list').innerHTML = '';
   // Create each li element (options)
   orderedOptions.forEach((elt) => {
-    let options = document.createElement("li");
-    options.setAttribute("id", "filters-options-" + elt);
-    options.classList.add("option");
-    options.setAttribute("aria-selected", "false");
-    options.setAttribute("role", "option");
-    options.setAttribute("tabindex", "0");
+    let options = document.createElement('li');
+    options.setAttribute('id', 'filters-options-' + elt);
+    options.classList.add('option');
+    options.setAttribute('aria-selected', 'false');
+    options.setAttribute('role', 'option');
+    options.setAttribute('tabindex', '0');
     options.innerHTML = `${elt}`;
-    document.getElementById("filters-options-list").append(options);
+    document.getElementById('filters-options-list').append(options);
     clickOnFilter(medias, photographerName, options, elt);
   });
 };
@@ -190,19 +193,19 @@ const clickOnFilter = (medias, photographerName, li, optionName) => {
   const handleFilters = (e) => {
     e.preventDefault();
     let orderedMedia = medias;
-    if (optionName == "Date") {
+    if (optionName == 'Date') {
       orderedMedia = orderByDate(e, medias);
       // Reorder the array to reorder option selection
-      filtersOptions = ["Date", "Popularité", "Titre"];
-    } else if (optionName == "Titre") {
+      filtersOptions = ['Date', 'Popularité', 'Titre'];
+    } else if (optionName == 'Titre') {
       orderedMedia = orderByTitle(e, medias);
-      filtersOptions = ["Titre", "Date", "Popularité"];
-    } else if (optionName == "Popularité") {
+      filtersOptions = ['Titre', 'Date', 'Popularité'];
+    } else if (optionName == 'Popularité') {
       orderedMedia = orderByPop(e, medias);
-      filtersOptions = ["Popularité", "Date", "Titre"];
+      filtersOptions = ['Popularité', 'Date', 'Titre'];
     }
     // Replace button inner text by the current applied filter
-    document.querySelector(".option-wrapper button span").textContent = optionName;
+    document.querySelector('.option-wrapper button span').textContent = optionName;
     displayGallery(orderedMedia, photographerName);
     Lightbox();
     buildOptionsList(orderedMedia, photographerName);
@@ -212,20 +215,22 @@ const clickOnFilter = (medias, photographerName, li, optionName) => {
     handleFilters(e)
   }
   // Filter when enter is press
-  li.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
+  li.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
       handleFilters(e);
+      optionWrapper.classList.remove('open');
+      optionList.classList.remove('open');
+    }
+  });
+  // Close filters if there is a click outside
+  window.addEventListener('click', function (e) {
+    if (!optionWrapper.contains(e.target)) {
+      optionWrapper.classList.remove('open');
+      optionList.classList.remove('open');
     }
   });
 };
 
-// Close filters if there is a click outside
-window.addEventListener("click", function (e) {
-  const select = document.querySelector(".option-wrapper");
-  if (!select.contains(e.target)) {
-    select.classList.remove("open");
-  }
-});
 
 // reorder media by date
 const orderByDate = (e, filteredMedia) => {
